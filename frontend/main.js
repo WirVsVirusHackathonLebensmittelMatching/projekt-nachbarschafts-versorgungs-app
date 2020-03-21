4ad13e994120d678c7a062089050253eb6bd3e49
Christoph Wendelin Leinen <c.leinen@sap.com>
2020-03-21 10:32:33 +0100

    Split app into Login and Overview

--- frontend/main.js
+++ frontend/main.js
@@ -1,99 +1,242 @@
 sap.ui.getCore().attachInit(function () {
   sap.ui.require([
+    "sap/m/App",
     "sap/m/Button",
-    "sap/m/Dialog",
     "sap/m/Input",
-    "sap/m/Label",
     "sap/m/VBox",
+    "sap/m/HBox",
     "sap/m/library",
-    "sap/ui/layout/form/SimpleForm",
-  ], function (Button, Dialog, Input, Label, VBox, library, SimpleForm) {
+    "sap/m/Page",
+    "sap/m/List",
+    "sap/m/MessageToast",
+    "sap/m/StandardListItem"
+  ], function (App, Button, Input, VBox, HBox, library, Page, List, MessageToast, StandardListItem) {
 
+    var handleRegisterPress = function () {
+      var oVornameInput = sap.ui.getCore().byId("vornameInput"),
+        oNachnameInput = sap.ui.getCore().byId("nachnameInput"),
+        oEmailInput = sap.ui.getCore().byId("emailInput"),
+        oPasswordInput = sap.ui.getCore().byId("passwordInput"),
+        oPasswordConfrimInput = sap.ui.getCore().byId("passwordConfirmInput");
+
+      if (!oVornameInput.getVisible()) {
+        oVornameInput.setVisible(true);
+        oNachnameInput.setVisible(true);
+        oPasswordConfrimInput.setVisible(true);
+      } else {
+        var bValid = true;
+
+        if (oVornameInput.getValue().length < 3) {
+          bValid = false;
+          oVornameInput.setValueState("Error");
+          oVornameInput.setValueStateText("Bitte gebe einen Vornamen ein.");
+        } else {
+          oVornameInput.setValueState("None");
+        }
+
+        if (oNachnameInput.getValue().length < 3) {
+          bValid = false;
+          oNachnameInput.setValueState("Error");
+          oNachnameInput.setValueStateText("Bitte gebe einen Nachnamen ein.");
+        } else {
+          oNachnameInput.setValueState("None");
+        }
+
+        if (oEmailInput.getValue().length < 3) {
+          bValid = false;
+          oEmailInput.setValueState("Error");
+          oEmailInput.setValueStateText("Bitte gebe eine Email Adresse ein.");
+        } else {
+          oEmailInput.setValueState("None");
+        }
+
+        if (oPasswordInput.getValue().length < 6) {
+          bValid = false;
+          oPasswordInput.setValueState("Error");
+          oPasswordInput.setValueStateText("Bitte gebe eine Password ein welches länger als 6 Zeichen ist!");
+        } else {
+          oPasswordInput.setValueState("None");
+        }
+
+        if (oPasswordConfrimInput.getValue() !== oPasswordInput.getValue()) {
+          bValid = false;
+          oPasswordConfrimInput.setValueState("Error");
+          oPasswordConfrimInput.setValueStateText("Passwörter stimmen nicht überein!");
+        } else {
+          oPasswordInput.setValueState("None");
+        }
+
+        if (bValid) {
+          MessageToast.show("RegistierungsMail wurde verschickt!");
+          oVornameInput.setVisible(false);
+          oNachnameInput.setVisible(false);
+          oPasswordConfrimInput.setVisible(false);
+        }
+      }
+    };
+
+    var handleLoginPress = function () {
+      var oEmailInput = sap.ui.getCore().byId("emailInput"),
+        oPasswordInput = sap.ui.getCore().byId("passwordInput"),
+        bValid = true;
+
+        if (oEmailInput.getValue().length < 3) {
+          bValid = false;
+          oEmailInput.setValueState("Error");
+          oEmailInput.setValueStateText("Bitte gebe eine Email Adresse ein.");
+        } else {
+          oEmailInput.setValueState("None");
+        }
+
+        if (oPasswordInput.getValue().length < 6) {
+          bValid = false;
+          oPasswordInput.setValueState("Error");
+          oPasswordInput.setValueStateText("Bitte gebe eine Password ein welches länger als 6 Zeichen ist!");
+        } else {
+          oPasswordInput.setValueState("None");
+        }
+
+        if (bValid && true) { // ask backend
+          sap.ui.getCore().byId("app").to("overview");
+        }
+    }
 
     var oVornameInput = new Input({
-      required: true
+      id: "vornameInput",
+      maxLength: 30,
+      placeholder: "Vorname",
+      visible: false,
+      width: "18rem"
     });
 
     var oNachnameInput = new Input({
-      required: true
+      id: "nachnameInput",
+      maxLength: 30,
+      placeholder: "Nachname",
+      visible: false,
+      width: "18rem"
     });
 
-    var oDialog = new Dialog({
-      title: "Registierung",
-      content: [
-        new Label({
-          text: "Vorname:",
-          labelFor: oVornameInput
-        }),
-        oVornameInput,
-        new Label({
-          text: "Nachname:",
-          labelFor: oNachnameInput
-        }),
-        oNachnameInput,
-        new Label({
-          text: "Email:"
-        }),
-        new Input()
-      ],
-      beginButton: [
-        new Button({
-          text: "Registieren",
-          type: "Emphasized",
-          press: function () {
-            oDialog.close();
-          }
-        })
-      ],
-      endButton: [
-        new Button({
-          text: "Schließen",
-          press: function () {
-            oDialog.close();
-          }
-        }),
-      ]
+    var oEmailInput = new Input({
+      id: "emailInput",
+      maxLength: 30,
+      placeholder: "E-mail",
+      type: library.InputType.Email,
+      width: "18rem"
     });
 
-    var oRegistrationButton = new Button({
-      text: "Registieren",
-      press: function () {
-        oDialog.open();
-      }
+    var oPasswordInput = new Input({
+      id: "passwordInput",
+      maxLength: 30,
+      placeholder: "Passwort",
+      type: "Password",
+      width: "18rem"
+    });
+
+    var oPasswordConfirmInput = new Input({
+      id: "passwordConfirmInput",
+      maxLength: 30,
+      placeholder: "Passwort wiederholen",
+      type: "Password",
+      visible: false,
+      width: "18rem"
     });
 
     var oLoginButton = new Button({
-      text: "Login",
-      type: "Accept",
+      id: "loginButton",
+      text: "Anmelden",
+      type: "Emphasized",
+      press: handleLoginPress
     });
 
-    var oEmailLoginInput = new Input({ required: true, type: library.InputType.Email });
-    var oEmailLoginLabel = new Label({
-      text: "Email:",
-      labelFor: oEmailLoginInput
-    });
-    var oPasswordLoginInput = new Input({ required: true, type: "Password" });
-    var oPasswordLoginLabel = new Label({
-      text: "Password:",
-      labelFor: oPasswordLoginInput
+    var oRegistrationButton = new Button({
+      id: "registerButton",
+      text: "Registieren",
+      press: handleRegisterPress
     });
 
-    var oForm = new SimpleForm({
-      layout: "ColumnLayout",
-      columnsXL: 2,
-      emptySpanXL:0,
-      content:
-        [
-          oEmailLoginLabel,
-          oEmailLoginInput,
-          oPasswordLoginLabel,
-          oPasswordLoginInput,
-          oLoginButton,
-          oRegistrationButton
-        ],
+    var oLoginPage = new Page({
+      id: "loginPage",
+      title: "Anmeldung",
+      content: [
+        new VBox({
+          justifyContent: "Center",
+          alignItems: "Center",
+          items: [
+            oVornameInput,
+            oNachnameInput,
+            oEmailInput,
+            oPasswordInput,
+            oPasswordConfirmInput,
+            new HBox({
+              // alignContent: "SpaceBetween",
+              // alignItems: "Stretch",
+              justifyContent: "SpaceBetween",
+              items: [
+                oLoginButton,
+                oRegistrationButton
+              ],
+              width: "18rem"
+            })
+          ]
+        })
+      ]
     });
 
-    oForm.placeAt("content");
+    var oOverviewPage = new Page({
+      id: "overview",
+      title: "Übersicht",
+      content: [
+        new List({
+          noDataText: "Es wurden keine Einkaufslisten gefunden.",
+          items: [
+            new StandardListItem({
+              icon: "sap-icon://nutrition-activity",
+              title: "Lebensmitteleinkauf",
+              description: "O-Saft, Brötchen, Wurst",
+              type: "Navigation",
+              iconInset: false,
+              info: "vor 2 Tagen"
+            }),
+            new StandardListItem({
+              icon: "sap-icon://pharmacy",
+              title: "Apoteheneinkauf",
+              description: "Desinfektionsmittel, Hustenbonbons",
+              type: "Navigation",
+              iconInset: false,
+              info: "vor 16 Stunden"
+            }),
+            new StandardListItem({
+              icon: "sap-icon://nutrition-activity",
+              title: "Lebensmitteleinkauf",
+              description: "Kornflakes, Milch, Kakao",
+              type: "Navigation",
+              iconInset: false,
+              info: "vor 5 Minuten"
+            }),
+            new StandardListItem({
+              icon: "sap-icon://activities",
+              title: "Anderer Einkauf",
+              description: "Katzenfutter, Katzenstreu",
+              type: "Navigation",
+              iconInset: false,
+              info: "vor wenigen Sekunden"
+            })
+          ]
+        })
+      ]
+      // to be done
+    });
+
+    var oApp = new App({
+      id: "app",
+      pages: [
+        oLoginPage,
+        oOverviewPage
+      ]
+    });
+
+    oApp.placeAt("content");
 
   });
 });
