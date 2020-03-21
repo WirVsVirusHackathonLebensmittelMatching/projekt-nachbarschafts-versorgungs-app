921de1e32bbc37a1a9283b7f9b5cc97a51b4c7ea
Mario Geckler <mario@mariogeckler.de>
2020-03-21 21:08:52 +0100

    Added user registration webservice call

--- frontend/views/loginPage.js
+++ frontend/views/loginPage.js
@@ -74,6 +74,28 @@ sap.ui.require([
       }
 
       if (bValid) {
+		var url = "http://localhost:8080/user-service/v1/users";
+
+		var data = {};
+		data.firstName = oVornameInput.getValue();
+		data.lastName  = oNachnameInput.getValue();
+		data.password = oPasswordInput.getValue();
+		data.mailAddress  = oEmailInput.getValue();
+		var json = JSON.stringify(data);
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("PUT", url, true);
+		xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+		xhr.onload = function () {
+			var users = JSON.parse(xhr.responseText);
+			if (xhr.readyState == 4 && xhr.status == "200") {
+				console.table(users);
+			} else {
+				console.error(users);
+			}
+		}
+		xhr.send(json);
+		  
         MessageToast.show("Registrierungsmail wurde verschickt!");
         oVornameInput.setVisible(false);
         oNachnameInput.setVisible(false);
